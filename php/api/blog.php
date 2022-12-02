163be7916c3344ff5c9a0be1f9d5b4d0ceefc5b2
SabahMawani <sabahmawani@gmail.com>
2022-12-02 19:44:33 +0500

    Blog updated

--- php/api/blog.php
+++ php/api/blog.php
@@ -8,7 +8,25 @@ $db = Database::getInstance();
 $method = $_SERVER['REQUEST_METHOD'];
 switch($method){
     case 'POST':
-
+        $data = json_decode( file_get_contents('php://input') );
+        $Heading = $data->Heading;
+        $post_by = $data->post_by;
+        $description = $data->description;
+        if($db->insert('blog',[
+            'Bid' => null,
+            'Heading' => $Heading,
+            'post_by' => $post_by,
+            'date_of_post' => date('Y-m-d'),
+            'description' => $description,
+        ]))
+        {
+            $response = ['status' => 1 , 'message'=>'Success'];
+        }
+        else{
+            $response = ['status' => 0 , 'message'=>'Failure'];
+        }
+        echo json_encode($response);
+        break;
 
     case 'GET':
         $results=$db->get('blog',['date_of_post','<=',date('Y-m-d')]);
@@ -20,4 +38,8 @@ switch($method){
             $response=['status'=>0,'results'=>$results->results(),'message'=>'Failure'];
         }
         echo json_encode($response);
+        break;
+    
+    case 'DELETE':
+        
 }
