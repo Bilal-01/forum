e0a59ef8f1baf4586a5af82f8835b74b86afe0a1
= <=>
2022-12-03 20:52:16 +0500

    society and event

--- php/forum.sql
+++ php/forum.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2022 at 08:33 PM
+-- Generation Time: Dec 03, 2022 at 04:23 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -40,7 +40,9 @@ CREATE TABLE `blog` (
 --
 
 INSERT INTO `blog` (`Bid`, `Heading`, `post_by`, `date_of_post`, `description`) VALUES
-(1, 'Bilal is an idiot', 'k200397', '2022-12-01', 'Why bilal is an idiot');
+(1, 'Lorem Ipsum', 'k200397', '2022-12-01', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras fringilla pretium mi a volutpat. Phasellus sed enim vitae justo feugiat venenatis at id purus. Aenean eros felis, egestas eget fringilla ac, fringilla vitae lectus. Nullam tincidunt at urna eu '),
+(2, 'Lorem ipsum', 'k200434', '2022-12-03', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras fringilla pretium mi a volutpat. Phasellus sed enim vitae justo feugiat venenatis at id purus. Aenean eros felis, egestas eget fringilla ac, fringilla vitae lectus. Nullam tincidunt at urna eu '),
+(3, 'Lorem ipsum', 'k200434', '2022-12-03', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras fringilla pretium mi a volutpat. Phasellus sed enim vitae justo feugiat venenatis at id purus. Aenean eros felis, egestas eget fringilla ac, fringilla vitae lectus. Nullam tincidunt at urna eu ');
 
 -- --------------------------------------------------------
 
@@ -91,6 +93,43 @@ INSERT INTO `course` (`cid`, `cname`, `coordinator`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `events`
+--
+
+CREATE TABLE `events` (
+  `event_id` int(11) NOT NULL,
+  `ename` varchar(100) NOT NULL,
+  `image_path` varchar(255) NOT NULL,
+  `sid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `events`
+--
+
+INSERT INTO `events` (`event_id`, `ename`, `image_path`, `sid`) VALUES
+(1, 'xyz', 'web/1.jpg', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `id` int(11) NOT NULL,
+  `full_name` varchar(55) NOT NULL,
+  `department` varchar(20) NOT NULL,
+  `domain` varchar(55) NOT NULL,
+  `skill` varchar(55) NOT NULL,
+  `about` varchar(500) NOT NULL,
+  `uid` varchar(7) NOT NULL,
+  `quote` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `project`
 --
 
@@ -107,7 +146,29 @@ CREATE TABLE `project` (
 --
 
 INSERT INTO `project` (`project_id`, `course_id`, `student_id`, `date_of_post`, `github_link`) VALUES
-(1, '123', 'k200397', '2022-12-02', 'github.com/xyz');
+(1, 'CS2001', 'k200397', '2022-12-02', 'github.com/xyz');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resource`
+--
+
+CREATE TABLE `resource` (
+  `ID` int(11) NOT NULL,
+  `course_id` varchar(10) NOT NULL,
+  `student_id` varchar(7) NOT NULL,
+  `Drive_Link` varchar(100) NOT NULL,
+  `date_of_publish` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `resource`
+--
+
+INSERT INTO `resource` (`ID`, `course_id`, `student_id`, `Drive_Link`, `date_of_publish`) VALUES
+(1, 'CS2001', 'k200397', 'https://drive.google.com/xyz', '2022-12-03'),
+(2, 'CS2002', 'k200434', 'https://drive.google.com/abc', '2022-12-03');
 
 -- --------------------------------------------------------
 
@@ -117,12 +178,19 @@ INSERT INTO `project` (`project_id`, `course_id`, `student_id`, `date_of_post`, 
 
 CREATE TABLE `society` (
   `society_id` int(11) NOT NULL,
-  `name` varchar(15) NOT NULL,
-  `description` varchar(50) NOT NULL,
+  `sname` varchar(150) NOT NULL,
+  `sdescription` varchar(500) NOT NULL,
   `head_id` int(11) NOT NULL,
   `president_id` varchar(7) NOT NULL,
   `socialMedia_link` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `society`
+--
+
+INSERT INTO `society` (`society_id`, `sname`, `sdescription`, `head_id`, `president_id`, `socialMedia_link`) VALUES
+(1, 'Dramatic and Extra Curricular Society', 'DECS has always been on the go in planning extra-activities such as picnics for the FASTians which prove to be both exhilarating and exclusive', 1, 'k200397', 'https://www.facebook.com/decsfast/');
 
 -- --------------------------------------------------------
 
@@ -143,7 +211,30 @@ CREATE TABLE `teacher` (
 
 INSERT INTO `teacher` (`tid`, `tname`, `email`, `location`) VALUES
 (1, 'Amin Sadiq', 'amin.sadiq@nu.edu.pk', 'CS Building, First Floor, Faculty Room'),
-(2, 'Danish Khan', 'danish.khan@nu.edu.pk', 'Don\'t go. Run away while you can!');
+(2, 'Danish Khan', 'danish.khan@nu.edu.pk', 'Don\'t go. Run away while you can!'),
+(3, 'Abdul Bari', 'abdul.bari@nu.edu.pk', 'EE First floor, Faculty Offices');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `timetable`
+--
+
+CREATE TABLE `timetable` (
+  `tt_id` int(11) NOT NULL,
+  `ttday` varchar(10) NOT NULL,
+  `tcode` varchar(20) NOT NULL,
+  `croom` varchar(10) NOT NULL,
+  `timeslot` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `timetable`
+--
+
+INSERT INTO `timetable` (`tt_id`, `ttday`, `tcode`, `croom`, `timeslot`) VALUES
+(1, 'Monday', 'Algo BSCS-5D', 'R-12', '08:8:50'),
+(2, 'Monday', 'DB BSCS-5D', 'R-109', '08:55-09:4');
 
 -- --------------------------------------------------------
 
@@ -153,6 +244,7 @@ INSERT INTO `teacher` (`tid`, `tname`, `email`, `location`) VALUES
 
 CREATE TABLE `user` (
   `id` varchar(7) NOT NULL,
+  `uname` varchar(55) NOT NULL,
   `email` varchar(17) NOT NULL,
   `password` varchar(55) NOT NULL,
   `role` int(11) NOT NULL
@@ -162,9 +254,9 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `email`, `password`, `role`) VALUES
-('k200397', 'k200397@nu.edu.pk', 'qwerty123456', 0),
-('k200434', 'k200434@nu.edu.pk', 'k20043491106', 0);
+INSERT INTO `user` (`id`, `uname`, `email`, `password`, `role`) VALUES
+('k200397', 'Bilal Aziz', 'k200397@nu.edu.pk', 'qwerty123456', 0),
+('k200434', 'Maryam Siddiqui', 'k200434@nu.edu.pk', 'k20043491106', 0);
 
 --
 -- Indexes for dumped tables
@@ -174,7 +266,8 @@ INSERT INTO `user` (`id`, `email`, `password`, `role`) VALUES
 -- Indexes for table `blog`
 --
 ALTER TABLE `blog`
-  ADD PRIMARY KEY (`Bid`);
+  ADD PRIMARY KEY (`Bid`),
+  ADD KEY `post_by` (`post_by`);
 
 --
 -- Indexes for table `carpool`
@@ -192,11 +285,34 @@ ALTER TABLE `course`
   ADD KEY `cid` (`cid`);
 
 --
+-- Indexes for table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`event_id`),
+  ADD KEY `event_society_FK` (`sid`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_profile_FK` (`uid`);
+
+--
 -- Indexes for table `project`
 --
 ALTER TABLE `project`
   ADD PRIMARY KEY (`project_id`),
-  ADD KEY `project_user_FK` (`student_id`);
+  ADD KEY `project_user_FK` (`student_id`),
+  ADD KEY `course_id` (`course_id`);
+
+--
+-- Indexes for table `resource`
+--
+ALTER TABLE `resource`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `course_id` (`course_id`,`student_id`),
+  ADD KEY `student_id` (`student_id`);
 
 --
 -- Indexes for table `society`
@@ -222,7 +338,8 @@ ALTER TABLE `timetable`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -232,7 +349,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `blog`
 --
 ALTER TABLE `blog`
-  MODIFY `Bid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `Bid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `carpool`
@@ -241,16 +358,34 @@ ALTER TABLE `carpool`
   MODIFY `carpool_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
+-- AUTO_INCREMENT for table `events`
+--
+ALTER TABLE `events`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
   MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
+-- AUTO_INCREMENT for table `resource`
+--
+ALTER TABLE `resource`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `society`
 --
 ALTER TABLE `society`
-  MODIFY `society_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `society_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `teacher`
@@ -262,11 +397,17 @@ ALTER TABLE `teacher`
 -- AUTO_INCREMENT for table `timetable`
 --
 ALTER TABLE `timetable`
-  MODIFY `tt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `tt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `blog`
+--
+ALTER TABLE `blog`
+  ADD CONSTRAINT `blog_ibfk_1` FOREIGN KEY (`post_by`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `carpool`
@@ -281,16 +422,36 @@ ALTER TABLE `course`
   ADD CONSTRAINT `course_teacher_fk` FOREIGN KEY (`coordinator`) REFERENCES `teacher` (`tid`);
 
 --
+-- Constraints for table `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `event_society_FK` FOREIGN KEY (`sid`) REFERENCES `society` (`society_id`);
+
+--
+-- Constraints for table `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `user_profile_FK` FOREIGN KEY (`uid`) REFERENCES `user` (`id`);
+
+--
 -- Constraints for table `project`
 --
 ALTER TABLE `project`
+  ADD CONSTRAINT `project_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`cid`),
   ADD CONSTRAINT `project_user_FK` FOREIGN KEY (`student_id`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `resource`
+--
+ALTER TABLE `resource`
+  ADD CONSTRAINT `resource_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `course` (`cid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `resource_ibfk_2` FOREIGN KEY (`student_id`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `society`
 --
 ALTER TABLE `society`
-  ADD CONSTRAINT `project_course_FK` FOREIGN KEY (`head_id`) REFERENCES `teacher` (`id`),
+  ADD CONSTRAINT `project_head_FK` FOREIGN KEY (`head_id`) REFERENCES `teacher` (`tid`),
   ADD CONSTRAINT `society_president_user_FK` FOREIGN KEY (`president_id`) REFERENCES `user` (`id`);
 COMMIT;
 
